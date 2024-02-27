b898aee294c949426428f1d514fd993e8b6159f3
mlorinc <xlorin01@vut.cz>
2024-02-27 23:13:08 +0100

    Refactor and add quantization generic methods for models
    Signed-off-by: mlorinc <xlorin01@vut.cz>

--- cmd/compress/models/base.py
+++ cmd/compress/models/base.py
@@ -1,23 +1,33 @@
+from typing import Tuple
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import random_split, DataLoader, Dataset
 from typing import Optional, Callable, Self
 from pathlib import Path
+import copy
 
 class BaseModel(nn.Module):
-    def __init__(self, model_path: str = None):
+    def __init__(self, model_path: str = None, model: nn.Module = None):
         super(BaseModel, self).__init__()
         self.model_path = model_path
 
-    def load(self, model_path: str):
-        model: nn.Module = torch.load(model_path)
+    def _prepare(self):
+        pass
 
-        if isinstance(model, nn.Module):
-            return model
-        else:
-            self.load_state_dict(model)
-            return self
+    def _convert(self):
+        pass
+
+    def load(self, model_path: str, quantized: bool = False):
+        model_state_dict: nn.Module = torch.load(model_path)
+
+        if quantized:
+            self.eval()
+            self._prepare()
+            self._convert()
+
+        self.load_state_dict(model_state_dict)
+        return self
 
     def save(self, path: str, save_model: bool = False):
         path = Path(path) if path is not None else Path(self.model_path)
@@ -30,13 +40,13 @@ class BaseModel(nn.Module):
 
     def clone(self) -> Self:
         # Create a new instance of the same type
-        clone = type(self)()
+        clone = copy.deepcopy(self)
         clone.load_state_dict(self.state_dict())
         return clone
 
-    def quantize(self):
-        torch.quantization.quantize_dynamic(self, dtype=torch.qint8, mapping=None, inplace=True)
-    
+    def quantize(self, new_path: str = None):
+        torch.quantization.quantize_dynamic(self, {torch.nn.Conv2d, torch.nn.Linear}, dtype=torch.qint8, mapping=None, inplace=True)
+
     def _fit_one_epoch(self, device: torch.device, train_loader: DataLoader, optimizer: optim.Optimizer, criterion: nn.modules.loss._WeightedLoss, batch_count: int = 1000):
         running_loss = 0.0
         last_loss = 0
@@ -56,7 +66,7 @@ class BaseModel(nn.Module):
 
             running_loss += loss.item()
 
-            if i % batch_count == batch_count - 1:
+            if batch_count is not None and i % batch_count == batch_count - 1:
                 # loss per batch
                 last_loss = running_loss / batch_count
                 print(f"\t Batch {i+1} loss: {last_loss}")
@@ -84,7 +94,8 @@ class BaseModel(nn.Module):
 
         # Number of epochs with no improvement before stopping
         patience_counter = 0
-        best_loss = float("inf")
+        best_validation_loss = float("inf")
+        best_train_loss = float("inf")
 
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # Fine-tune the model for a few epochs
@@ -105,11 +116,13 @@ class BaseModel(nn.Module):
                     validation_loss += loss.item()
 
             average_validation_loss = validation_loss / (i + 1)
-            print(f"\ttrain loss: {(running_loss / batch_count):.12f}, validation Loss: {average_validation_loss:.12f}")
+            average_train_loss = running_loss / batch_count
+            print(f"\ttrain loss: {average_train_loss:.12f}, validation Loss: {average_validation_loss:.12f}")
 
-            if average_validation_loss < best_loss:
+            best_train_loss = min(average_train_loss, best_train_loss)
+            if average_validation_loss < best_validation_loss:
                 patience_counter = 0
-                best_loss = average_validation_loss
+                best_validation_loss = average_validation_loss
                 if on_improve:
                     on_improve(average_validation_loss)
             else:
@@ -121,38 +134,50 @@ class BaseModel(nn.Module):
                 break
 
         print(f"Finished training of {self.__class__.__name__}")
+        return best_train_loss, best_validation_loss
 
     def _evaluate(self,
                  dataset: Dataset,
                  criterion: nn.modules.loss._WeightedLoss,
-                 batch_size: int = 32
+                 batch_size: int = 32,
+                 max_batches: int = None,
+                 top: int = 1
                  ):
         original_train_mode = self.training
         try:
             self.eval()
             loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+            running_loss = 0
+            total_samples = 0
+            running_correct = 0
             with torch.no_grad():
-                running_loss = 0
-                total = 0
-                correct = 0
-                for data in loader:
-                    x, y = data
+                for batch_index, (x, y) in enumerate(loader):
                     y_hat = self(x)
                     loss = criterion(y_hat, y)
-                    running_loss = loss.item()
-                    _, predicted = torch.max(y_hat.data, 1)
-                    total += y.size(0)
-                    correct += (predicted == y).sum().item()
-            acc = 100 * correct / total
-            test_loss = running_loss / len(loader)
+                    loss = loss.item() * y.size(0)
+                    running_loss += loss
+                    _, predicted = torch.max(y_hat, 1)
+                    total_samples += y.size(0)
+                    correct = (predicted == y).sum().item()
+                    running_correct += correct 
+                    
+                    if batch_index % 100 == 0:
+                        print(f"batch {batch_index} acc: {100 * correct / y.size(0):.12f}%, loss: {loss / y.size(0):.12f}")
+                    if max_batches is not None and batch_index >= max_batches:
+                        break
+
+            acc = 100 * running_correct / total_samples
+            test_loss = running_loss / total_samples
             return acc, test_loss
+        except Exception as e:
+            raise e
         finally:
             self.train(mode=original_train_mode)
 
-    def fit(self, batch_size: int = 32):
+    def fit(self, batch_size: int = 32) -> Tuple[float, float]:
         raise NotImplementedError()
 
-    def evaluate(self, batch_size: int = 32):
+    def evaluate(self, batch_size: int = 32, max_batches: int = None):
         raise NotImplementedError()
     
 def init(model_path: Optional[str]) -> BaseModel:
--- cmd/compress/models/lenet.py
+++ cmd/compress/models/lenet.py
@@ -2,6 +2,7 @@ from typing import Optional
 from models.base import BaseModel
 import torch.nn as nn
 import torch
+from torch.utils.data import random_split, DataLoader
 import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
@@ -19,6 +20,7 @@ class LeNet5(BaseModel):
         self.fc1 = nn.Linear(16 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
     def _on_improve(self, average_validation_loss: float):
         self.save(self.model_path)
@@ -36,23 +38,40 @@ class LeNet5(BaseModel):
         return x
 
     def fit(self):
-        # Define transformations and load the MNIST dataset
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
         # Load MNIST dataset
-        dataset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+        dataset = self._get_train_data()
         # Define loss function and optimizer
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
-        self._fit(dataset=dataset, optimizer=optimizer, criterion=criterion, on_improve=self._on_improve, batch_count=500, batch_size=16, epochs=100, patience=10)
+        train_loss, val_loss = self._fit(dataset=dataset, optimizer=optimizer, criterion=criterion, on_improve=self._on_improve, batch_count=500, batch_size=16, epochs=100, patience=10)
+        return train_loss, val_loss
 
-    def evaluate(self, batch_size: int = 16):
-        # Define transformations and load the MNIST dataset
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    def evaluate(self, batch_size: int = 16, max_batches = None):
         # Load MNIST dataset
-        dataset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+        dataset = self._get_test_data()
         # Define loss function and optimizer
         criterion = nn.CrossEntropyLoss()
-        return self._evaluate(dataset=dataset, criterion=criterion, batch_size=batch_size)
+        return self._evaluate(dataset=dataset, criterion=criterion, batch_size=batch_size, max_batches=max_batches)
+
+    def _get_train_data(self):
+        return torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=self.transform)
+
+    def _get_test_data(self):
+        return torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=self.transform)
+
+    def _get_train_validation_data(self,
+              batch_size: int = 32,
+              shuffle: bool = True,
+              validation_dataset_ratio: float = 0.2):
+        dataset = self._get_train_data()
+        dataset_size = len(dataset)
+        validation_set_size = int(validation_dataset_ratio * dataset_size)
+        training_set_size = dataset_size - validation_set_size
+        train_set, validation_set = random_split(dataset, [training_set_size, validation_set_size])
+
+        train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=shuffle)
+        validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
+        return train_loader, validation_loader
 
 def init(model_path: Optional[str]) -> LeNet5:
     return LeNet5(model_path)
