3d59e5071f0954f625493086b2bbe7d97ccf413d
mlorinc <xlorin01@vut.cz>
2024-03-31 20:40:05 +0200

    Add chain experiment
    Signed-off-by: mlorinc <xlorin01@vut.cz>

---
+++ cmd/compress/experiments/chain/experiment.py
@@ -0,0 +1,26 @@
+import torch
+from cgp.cgp_adapter import CGP
+from models.base import BaseModel
+from experiments.experiment import BaseExperiment, conv2d_core, conv2d_outter
+
+class ChainExperiment(BaseExperiment):
+    name = "chain"
+    def __init__(self, experiment_folder: str, model: BaseModel, cgp: CGP, dtype=torch.int8) -> None:
+        super().__init__(experiment_folder, ChainExperiment.name, model, cgp, dtype)
+        self.add_filters("conv1", 
+            [
+                conv2d_core([0, slice(None)], 5, 3)
+            ], 
+            [
+                *conv2d_outter([0, slice(None)], 5, 3)
+            ])
+        self.add_filters("conv2", 
+            [
+                conv2d_core([slice(None), 0], 5, 3)
+            ], 
+            [
+                *conv2d_outter([slice(None), 0], 5, 3)
+            ])
+
+def init(experiment_folder: str, model: BaseModel, cgp: CGP, dtype=torch.int8):
+    return ChainExperiment(experiment_folder, model, cgp, dtype)
