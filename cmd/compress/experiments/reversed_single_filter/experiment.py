7165adb3c87afc60aed1366fb259420acf4233ee
mlorinc <xlorin01@vut.cz>
2024-03-31 20:40:17 +0200

    Add Reverse Single Filter experiment
    Signed-off-by: mlorinc <xlorin01@vut.cz>

---
+++ cmd/compress/experiments/reversed_single_filter/experiment.py
@@ -0,0 +1,19 @@
+import torch
+from cgp.cgp_adapter import CGP
+from models.base import BaseModel
+from experiments.experiment import BaseExperiment, conv2d_core, conv2d_outter
+
+class ReversedSingleFilterExperiment(BaseExperiment):
+    name = "reversed_single_filter"
+    def __init__(self, experiment_folder: str, model: BaseModel, cgp: CGP, dtype=torch.int8) -> None:
+        super().__init__(experiment_folder, ReversedSingleFilterExperiment.name, model, cgp, dtype)
+        self.add_filters("conv1", 
+            [
+                *conv2d_outter([0, 0], 5, 3)
+            ], 
+            [
+                conv2d_core([0, 0], 5, 3)
+            ])
+
+def init(experiment_folder: str, model: BaseModel, cgp: CGP, dtype=torch.int8):
+    return ReversedSingleFilterExperiment(experiment_folder, model, cgp, dtype)
